81445eec0cb615fa862d2d341e3c01e7daaa9ee3
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2024-11-05 17:29:12 +0800

    AdminFrontDev - USER 01 admin done

--- src/views/modules/user/admin/index.vue
+++ src/views/modules/user/admin/index.vue
@@ -6,52 +6,8 @@
       :option="tableOption"
       :page="page"
       @search-change="onSearch"
-      @selection-change="selectionChange"
       @on-load="getDataList"
     >
-      <template #menu-left>
-        <el-button
-
-          icon="el-icon-plus"
-          type="primary"
-          @click.stop="onAddOrUpdate()"
-        >
-          新增
-        </el-button>
-
-        <el-button
-
-          :disabled="dataListSelections.length <= 0"
-
-
-          @click="onDelete()"
-        >
-          批量删除
-        </el-button>
-      </template>
-
-      <template
-        #menu="scope"
-      >
-        <el-button
-
-          icon="el-icon-edit"
-          type="primary"
-          @click.stop="onAddOrUpdate(scope.row.userId)"
-        >
-          编辑
-        </el-button>
-
-        <el-button
-
-          icon="el-icon-delete"
-
-          @click.stop="onDelete(scope.row.userId)"
-        >
-          删除
-        </el-button>
-      </template>
-
     </avue-crud>
 
     <!-- 弹窗 新增 / 修改 -->
@@ -65,98 +21,45 @@
 </template>
 
 <script setup>
-import {ElMessage, ElMessageBox} from 'element-plus'
 
 const dataList = ref([])
-const dataListLoading = ref(false)
-const dataListSelections = ref([])
-const addOrUpdateVisible = ref(false)
+
 const page = reactive({
-  total: 0, // 总页数
-  currentPage: 1, // 当前页数
-  pageSize: 10 // 每页显示多少条
+  currentPage: 1
 })
 
 /**
  * 获取数据列表
  */
 const getDataList = (pageParam, params, done) => {
-  dataListLoading.value = true
   http({
-    url: http.adornUrl('/sys/user/page'),
+    url: http.adornUrl('/admin/employee/page'),
     method: 'get',
     params: http.adornParams(
       Object.assign(
         {
-          current: pageParam == null ? page.currentPage : pageParam.currentPage,
-          size: pageParam == null ? page.pageSize : pageParam.pageSize
+          current: pageParam == null ? page.currentPage : pageParam.currentPage
         },
         params
       )
     )
-  }).then(({data}) => {
-    dataList.value = data.records
-    page.total = data.total
-    dataListLoading.value = false
+  }).then((response) => {
+    dataList.value = response.data.data.records
     if (done) done()
   })
 }
+
+
 /**
- * 条件查询
+ * 搜索时候拉数据
  */
 const onSearch = (params, done) => {
   getDataList(page, params, done)
 }
-/**
- * 多选变化
- */
-const selectionChange = (val) => {
-  dataListSelections.value = val
-}
-
-const addOrUpdateRef = ref(null)
 
 /**
- * 新增 / 修改
+ * 表格配置
  */
-const onAddOrUpdate = (id) => {
-  addOrUpdateVisible.value = true
-  nextTick(() => {
-    addOrUpdateRef.value?.init(id)
-  })
-}
-
-/**
- * 删除
- */
-const onDelete = (id) => {
-  const userIds = id ? [id] : dataListSelections.value?.map(item => {
-    return item.userId
-  })
-  ElMessageBox.confirm(`确定对[id=${userIds.join(',')}]进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
-    confirmButtonText: '确定',
-    cancelButtonText: '取消',
-    type: 'warning'
-  }).then(() => {
-    http({
-      url: http.adornUrl('/sys/user'),
-      method: 'delete',
-      data: http.adornData(userIds, false)
-    }).then(() => {
-      ElMessage({
-        message: '操作成功',
-        type: 'success',
-        duration: 1500,
-        onClose: () => {
-          getDataList()
-        }
-      })
-    })
-  }).catch(() => {
-  })
-}
-
-
 const tableOption = {
   searchMenuSpan: 6,
   columnBtn: false,
@@ -179,32 +82,13 @@ const tableOption = {
     value: 'value'
   },
   column: [{
-    label: '用户名',
-    prop: 'username',
-    search: true
+    label: '账号',
+    prop: 'account',
+    search: false
   }, {
-    label: '邮箱',
-    prop: 'email'
-  }, {
-    label: '手机号',
-    prop: 'mobile'
-  }, {
-    label: '创建时间',
-    prop: 'createTime'
-  }, {
-    label: '状态',
-    prop: 'status',
-    type: 'select',
-    dicData: [
-      {
-        label: '禁用',
-        value: 0
-      }, {
-        label: '正常',
-        value: 1
-      }
-    ]
-
+    label: '姓名',
+    prop: 'name',
+    search: false
   }]
 }
 
